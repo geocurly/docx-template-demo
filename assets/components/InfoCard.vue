bffc5f9c243d0c29b54ee982d4ec3a2170b428ab
aleksandr.blizorukov <geocurly@gmail.com>
2020-11-16 22:04:03 +0300

    Ast Highlight

--- assets/components/InfoCard.vue
+++ assets/components/InfoCard.vue
@@ -1,11 +1,22 @@
 <script>
+    import formatHighlight from 'json-format-highlight';
     import TemplateCard from "./TemplateCard";
     export default {
         name: "InfoCard",
         extends: TemplateCard,
         computed: {
             content: function() {
-                return JSON.stringify(this.$store.getters.ast, null, 2);
+                return formatHighlight(
+                    this.$store.getters.ast,
+                    {
+                        numberColor: "#80D8FF",
+                        stringColor: "#8BC34A",
+                        trueColor: "#EF6C00",
+                        falseColor: "#EF6C00",
+                        nullColor: "#EF6C00",
+                        keyColor: "#B39DDB",
+                    }
+                );
             }
         },
         data: () => ({
--- assets/components/TemplateCard.vue
+++ assets/components/TemplateCard.vue
@@ -5,8 +5,8 @@
         </template>
         <v-card-title>{{ title }}</v-card-title>
         <v-divider class="mx-4"></v-divider>
-        <v-card-text>
-            <pre :contenteditable="editable" ref="pre">{{content}}</pre>
+        <v-card-text class="content">
+            <pre :contenteditable="editable" ref="pre"><div v-html="content"/></pre>
         </v-card-text>
     </v-card>
 </template>
@@ -22,5 +22,10 @@
     }
 </script>
 
-<style scoped lang="scss">
+<style>
+    [contenteditable] {
+        outline: 0 solid transparent;
+        caret-color: #E1F5FE;
+        white-space: pre-wrap;
+    }
 </style>
